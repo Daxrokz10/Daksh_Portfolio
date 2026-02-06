8f42e62dbe978bd04b1f16bb15fc7bada0a1fe6d
Daxrokz10 <dakshgagnani@gmail.com>
2026-02-06 09:58:42 +0530

    added text

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import Particles from '../bits/Particles.jsx';
+import TextType from '../bits/TextType.jsx';
+
 const Home = () => {
     return (
         <div className='home'>
-            <h1>Hello</h1>
+            <div className="home-content text-center">
+                <div className="home-content-card">
+                    
+                </div>
+                <div className="home-content-text">
+                    <TextType
+                        text={["Text typing effect", "for your websites", "Happy coding!"]}
+                        typingSpeed={75}
+                        pauseDuration={1500}
+                        showCursor
+                        cursorCharacter="_"
+                        texts={["Welcome to React Bits! Good to see you!", "Build some amazing experiences!"]}
+                        deletingSpeed={50}
+                        variableSpeedEnabled={false}
+                        variableSpeedMin={60}
+                        variableSpeedMax={120}
+                        cursorBlinkDuration={0.5}
+                    />
+                </div>
+            </div>
             <div style={{ width: '100%', height: '600px', position: 'relative' }}>
                 <Particles
                     particleColors={["#b19eef"]}
